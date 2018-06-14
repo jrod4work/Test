dc1882fa792788b89c62b9facf6be85e0ce40a4d
Unknown <jrod4work@hotmail.com>
2018-06-14 08:35:42 -0600

    Change questions

--- java2.js
+++ java2.js
@@ -9,11 +9,11 @@ document.getElementById("form1").onsubmit=function() {
 	   
     document.getElementById("grade").innerHTML = result;
     
-if (result === 0) {result2 = "I don't think you studied."};
-if (result === 25) {result2 = "You need to spend more time. Try again."};
-if (result === 50) {result2 = "I think you could do better. Try again."};
-if (result === 75) {result2 = "So close. Try again."};
-if (result === 100) {result2 = "Excellent! You're a JavaScript pro!"};
+if (result === 0) {result2 = "Keep Googling!"};
+if (result === 25) {result2 = "You need to spend more time on the Google site"};
+if (result === 50) {result2 = "I think you could do better. Google again."};
+if (result === 75) {result2 = "So close. Try another search."};
+if (result === 100) {result2 = "Excellent! You're a Google Pro!"};
 document.getElementById("grade2").innerHTML = result2; 
 
 
