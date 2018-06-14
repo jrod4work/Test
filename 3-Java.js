1aea2a061fcc9e9d086ef617e29fb5ce17cc7c59
Unknown <jrod4work@hotmail.com>
2018-06-14 16:12:23 -0600

    Editing 3rd page

--- 3-Java.js
+++ 3-Java.js
@@ -1,12 +1,35 @@
 var cities = [
-    {name: "Austin", Population: 842},
-    {name: "Fort Worth", Population: 777},
-    {name: "Lubbock", Population: 236}
+    {name: "Austin", Population: 842, Units: 354, Households: 322, tip: "live music capital"},
+    {name: "Fort Worth", Population: 777, Units: 291, Households: 257, tip: "near Dallas"},
+    {name: "Lubbock", Population: 236, Units: 95, Households: 86, tip: "TX Tech"}
     ];
+    
+    var measures = Object.keys(cities[0]);
 
-    for (var i = 0; i<cities.length; i++) {
-        document.getElementById("name" + i).innerHTML= cities[i].name;
-        document.getElementById("bartext" + i).innerHTML= cities[i].Population;
-        document.getElementById("bar" + i).style.width = cities[i].Population/10 + "%";
-        };
-        
+    
+    function writeData(x) {
+        for (var i = 0; i<cities.length; i++) {
+        document.getElementById("name" + (i)).innerHTML= eval("cities[i]." + measures[0]);
+        document.getElementById("bartext" + (i)).innerHTML= eval("cities[i]." + measures[x]);
+        document.getElementById("bar" + (i)).style.width = eval("cities[i]." + measures[x])/10 + "%";
+        document.getElementById("bar" + (i)).title= eval("cities[i]." + measures[4]); 
+        }
+        //city and tip do not need to change for each measure. 
+    
+        // this area inserts the Category as a heading 
+        document.getElementById("cat").innerHTML = "<strong>" + measures[x] + "</strong>";
+    };
+    
+    writeData(1); // this is where we call the function the first time. 
+    
+    // function to change based on dropdown
+function selMeasure() { // function that writes the data based on the dropdown selection
+    var m = document.getElementById("sel").value;
+    writeData(m); 
+    };
+    
+    $( document ).ready(function() {
+        $("[title]").tooltip();
+         });
+
+         $("[title]").tooltip({offset: [10, -30]});
